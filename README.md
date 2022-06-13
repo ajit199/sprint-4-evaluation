5d2de84194328a9dc4b4fc82154f87b6d855168c
Ajit Singh <79194669+ajit199@users.noreply.github.com>
2022-06-13 17:26:37 +0530

    done

---
+++ README.md
@@ -0,0 +1,1 @@
+# rct20104evaluation
--- index.js
+++ index.js
@@ -6,7 +6,11 @@ let app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/news",authRouter)
-app.listen(process.env.PORT || 8080, async () => { 
+app.get("/",(req,res)=>{
+  res.send("App Working")
+})
+let PORT = process.env.PORT || 8080;
+app.listen(PORT, async () => { 
     await connection;
     console.log("db connected");
   console.log("server started successfully!");
